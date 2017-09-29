53b42edb62f6bbd5aefee0c1217978afdae7b87e
MathieuRobilliard <mathieu.robilliard@gmail.com>
2017-09-29 23:53:20 +0200

    [feat] simple rooting ready
    The file index.tsx includs a ReactDOM.render element with a Switch element. Then, each Route is defined with a given path and a given name (the name from the import).

---
+++ src/LoginPage.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class LoginPage extends React.Component {
+    render() {
+      return (
+        <div >
+            LoginPage
+        </div>
+      );
+    }
+  }
+  
+  export default LoginPage;
---
+++ src/index.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+//import { Router, Route, hashHistory } from 'react-router'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import App from './App';
+import LoginPage from './LoginPage';
+
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+/*
+ReactDOM.render(
+  <App />,  //Element
+  document.getElementById('root') as HTMLElement  //container
+  //callback 
+);
+registerServiceWorker();
+*/
+ReactDOM.render(
+  <BrowserRouter>
+  <Switch>
+      <Route path="/App" component={ App }/>
+      <Route path="/Login" component={ LoginPage }/>
+  </Switch>
+  </BrowserRouter>, document.getElementById('root')
+);
+registerServiceWorker();
+/*
+ReactDOM.render(
+  <Router history={BrowserRouter}>
+    <Route path="/" component={App}>
+    </Route>
+    <Route path="/Login" component={LoginPage}>
+    </Route>
+  </Router>,
+  document.getElementById('root') as HTMLElement  //container
+);
+registerServiceWorker();
+*/
