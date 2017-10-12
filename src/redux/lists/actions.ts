d0eb33a1929a7b162b0f280a575759f265eedb54
YvesLongchamp <longchampyves84@gmail.com>
2017-10-12 18:23:58 +0200

    feat: add a bit of actions into the lists

--- src/redux/lists/actions.ts
+++ src/redux/lists/actions.ts
@@ -1,28 +1,100 @@
-export const ADD_LIST = 'ADD_LIST'
+import { IAttachment } from '../attachments/types'
+import { IComment } from '../comments/types'
+import { ITasksList } from '../taskLists/types'
+import { ITag } from '../tags/types'
+import { ICard } from '../cards/types'
+
+export const LIST_SUCCESS = 'LIST_SUCCESS'
+export const LIST_ERROR = 'LIST_ERROR'
+
+export const ADD_ELEMENT_LIST = 'ADD_ELEMENT_LIST'
+export const ADD_ELEMENT_LIST_SUCCESS = 'ADD_ELEMENT_LIST_SUCCESS'
+
+export const REMOVE_ELEMENT_LIST = 'REMOVE_ELEMENT_LIST'
+export const REMOVE_ELEMENT_LIST_SUCCESS = 'REMOVE_ELEMENT_LIST_SUCCESS'
+
+export const UPDATE_INFORMATIONS_LIST = 'UPDATE_INFORMATIONS_LIST'
+
+export const CREATE_LIST = 'CREATE_LIST'
 export const REMOVE_LIST = 'REMOVE_LIST'
 
 export type Actions = {
-    ADD_LIST: {
-        type: typeof ADD_LIST,
-        id: number,
-        title: string,
-        rank: string,
+
+    LIST_SUCCESS: {
+        type: typeof LIST_SUCCESS,
+        message: string,
     },
-    REMOVE_LIST: {
-        type: typeof REMOVE_LIST,
+    LIST_ERROR: {
+        type: typeof LIST_ERROR,
+        error: string,
+    },
+
+    CREATE_LIST: {
+        type: typeof CREATE_LIST,
+        title: string, 
+        rank: number,
+        cards: ICard[],
+    },
+
+    ADD_ELEMENT_LIST: {
+        type: typeof ADD_ELEMENT_LIST,
+        id: number,
+        description: string, 
+        dueTime: Date,
+        attachments: IAttachment[],
+        comments: IComment[],
+        tasksLists: ITasksList[],
+        tags: ITag[],
+    },
+    ADD_ELEMENT_LIST_SUCCESS: {
+        type: typeof ADD_ELEMENT_LIST_SUCCESS,
+        id: number,
+        description: string, 
+        dueTime: Date,
+        attachments: IAttachment[],
+        comments: IComment[],
+        tasksLists: ITasksList[],
+        tags: ITag[],
+    },
+
+    REMOVE_ELEMENT_LIST: {
+        type: typeof REMOVE_ELEMENT_LIST,
         index: number,
     },
+    REMOVE_ELEMENT_LIST_SUCCESS: {
+        type: typeof REMOVE_ELEMENT_LIST_SUCCESS,
+        id: number,
+        title: string, 
+        rank: number,
+        cards: ICard[],
+    },
+
+    UPDATE_INFORMATIONS_LIST: {
+        type: typeof UPDATE_INFORMATIONS_LIST,
+        title: string, 
+        rank: number,
+        cards: ICard[],
+    },
+
 }
 
 export const actionCreators = {
-    addList: (id: number, title: string, rank: string): Actions[typeof ADD_LIST] => ({
-        type: ADD_LIST,
-        id,
+    createList: (title: string, rank: number, cards: ICard[]): Actions[typeof CREATE_LIST] => ({
+        type: CREATE_LIST,
         title,
         rank,
+        cards,
     }),
-    remove: (index: number): Actions[typeof REMOVE_LIST] => ({
-        type: REMOVE_LIST,
+    UpdateInformationsList: (   title: string, 
+                                rank: number, 
+                                cards: ICard[]): Actions[typeof UPDATE_INFORMATIONS_LIST] => ({
+        type: UPDATE_INFORMATIONS_LIST,
+        title,
+        rank,
+        cards
+    }),
+    removeElementFromList: (index: number): Actions[typeof REMOVE_ELEMENT_LIST] => ({
+        type: REMOVE_ELEMENT_LIST,
         index,
     }),
 }
--- src/redux/lists/reducers.ts
+++ src/redux/lists/reducers.ts
@@ -2,24 +2,36 @@ import { ADD_LIST, REMOVE_LIST } from './actions'
 import { RootAction } from '../RootAction'
 import { IList } from '../lists/types'
 
-export type State = IList[]
+export type State = IList
 
-const defaultValue: State = []
+const defaultValue: State = {
+    id: -1,
+    title: '',
+    rank: -1,
+    cards: [],    
+}
 
 export const reducer = (state: State = defaultValue, action: RootAction) => {
         switch (action.type) {
 
-            case ADD_LIST:
-                return [...state, {
+            case ADD_LIST: 
+                state.cards = [...state.cards, {
                     id: action.id,
-                    title: action.title,
+                    description: action.description, 
+                    dueTime: action.dueTime,
+                    attachments: action.attachments,
+                    comments: action.comments,
+                    tasksLists: action.tasksLists,
+                    tags: action.tags,
                 }]
+                return state
 
             case REMOVE_LIST:
-                return [
-                    ...state.slice(0, action.index),
-                    ...state.slice(action.index + 1)
+                state.cards = [
+                    ...state.cards.slice(0, action.index),
+                    ...state.cards.slice(action.index + 1)
                 ]
+                return state
 
             default:
                 return state
